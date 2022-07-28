45f35290841c853fa762db6382fdb5bf8646e683
GiammarcoBozzelli <giammarcobozzelli3006@gmail.com>
2022-07-28 13:46:26 +0200

    modifies check_input

--- check.py
+++ check.py
@@ -1,8 +1,11 @@
+import re
+
+
 def check_input(args):
     '''Checks that the input food is made of only alphanumeric characters'''
-    if not args.food.isalpha():
+    if re.search(args.food.lower(), ['a-z ,']):
+        return True
+    else:
         print('There is something wrong with your food name... Try with'
               + ' something like \'Apple\'')
-        return(False)
-
-    return True
+        return False
