c9f55b0469c5743aab876cb7181811924cec9dea
Chiara Dalto <chiaradalto@MacBook-Air-di-Chiara-2.local>
2022-07-29 19:56:13 -0400

    new function created

--- CleanDataProva.py
+++ CleanDataProva.py
@@ -7,8 +7,28 @@ def read_dataframe(file_name):
 
     return df.copy()
 
+df = read_dataframe('nutrition.csv')
+print(df.columns)
+
+
 
 def search_coordinate(df_data: pd.DataFrame, search_set: set) -> list:
     nda_values = df_data.values
     tuple_index = np.where(np.isin(nda_values, [e for e in search_set]))
     return [(row, col, nda_values[row][col]) for row, col in zip(tuple_index[0], tuple_index[1])]
+
+def get_info(verbose, show, add, protein, calories, carbs, fats):
+
+    if verbose == True:
+
+    if show == True:
+
+    if add == True:
+
+    if protein == True:
+
+    if calories == True:
+
+    if carbs == True:
+
+    if fats == True:
