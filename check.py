08573aa7c808d2d99f1c8852952026384fc04b2a
GiammarcoBozzelli <giammarcobozzelli3006@gmail.com>
2022-07-29 12:08:35 +0200

    modified check.py

--- check.py
+++ check.py
@@ -1,10 +1,12 @@
 
+from ast import Break
+
+
 def check_input(args):
     '''Checks that the input food is made of only accepted characters'''
-    while True:
-        if args.food.isalpha():
-            return True
-        else:
-            print('There is something wrong with your food name... Try with'
-                  + ' something like \'Apple\'')
-            return False
+    if args.food.isalpha():
+        return True
+    else:
+        print('There is something wrong with your food name... Try again '
+              + 'with something like \'Apple\'')
+        return False
