5bb859a8a71a87c7ac345f8b7fe0d25684a5cda5
GiammarcoBozzelli <giammarcobozzelli3006@gmail.com>
2022-07-31 11:14:08 +0200

    Modified main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     # and controls if the input is valid
     arguments = parse_arguments()
     if check_input(arguments):
-        arguments.food = ''.join(arguments.food)
+        arguments.food = ''.join(arguments.food).lower()
         print(arguments)
         # get_info(arguments)  # check if the food is in the dataset
     else:
