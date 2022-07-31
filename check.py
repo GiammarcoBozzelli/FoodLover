fe9fa8e4309eb24187e6098d9df46d414dc35174
GiammarcoBozzelli <giammarcobozzelli3006@gmail.com>
2022-07-31 11:12:10 +0200

    Modified check.py and main.py

--- check.py
+++ check.py
@@ -1,11 +1,12 @@
 
 
 def check_input(args):
-    '''Checks that the input food is made of only accepted characters'''
+    '''Checks that the input food is made of only accepted characters
+       if not, it returns an error message to the user'''
     if ''.join(args.food).isalpha():
         return True
     else:
         print('There is something wrong with your food name... Try again '
               + 'with something like \'Apple\' and avoid using special'
-              + ' characters!')
+              + ' characters or numbers!')
         return False
--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from check import check_input
 if __name__ == '__main__':
 
     # Retrieve user input from terminal
+    # and controls if the input is valid
     arguments = parse_arguments()
     if check_input(arguments):
+        arguments.food = ''.join(arguments.food)
         print(arguments)
         # get_info(arguments)  # check if the food is in the dataset
     else:
