1b2a9a80101cddf3f765bd2b9ec94997f36899b8
Angelica Habib <angelicahabib@Mac-di-Angelica.local>
2022-08-06 15:21:33 +0200

    issue #8

--- CleanDataProva.py
+++ CleanDataProva.py
@@ -17,25 +17,29 @@ def read_dataframe(file_name):
 
     return df.copy()
 
-def get_info(df, food, verbose, show, add, protein, calories, carbs, fats):
-    row = df.loc[df['name'] == food] #ottengo la riga corrispondente al food specificato dall'utente
-    output = ""
+def get_info(arguments):
+    df = read_dataframe('nutrition.csv')
+    row = df.loc[df['name'] == arguments.food] # ottengo la riga corrispondente al food specificato dall'utente
+    output = f"Name: {arguments.food}\n"
     # TODO: rimuovere pass una volta scritto il codice all'interno del blocco IF
-    if verbose == True:
+    if arguments.show == True:
         pass
-    if show == True:
-        pass
-    if add == True:
-        pass
-    if protein == True:
+    if arguments.add == True:
+        update_dataframe(df)
+    if arguments.protein == True:
         output += "protein: " + str(row["protein"].values[0]) + "\n"
-    if calories == True:
-        output += "calories: " + str(row["calories"].values[0]) + "\n"
-    if carbs == True:
+    if arguments.calories == True:
+        output += "calories: " + str(row["calories"].values[0]) + ' kcal' + "\n"
+    if arguments.carbs == True:
         output += "carbs: " + str(row["carbohydrate"].values[0]) + "\n"
-    if fats == True:
+    if arguments.fats == True:
         output += "fats: " + str(row["total_fat"].values[0]) + "\n"
-    return output
+    if not arguments.add and not arguments.protein and not arguments.carbs and not arguments.fats and not arguments.calories and not arguments.show:
+        output += "protein: " + str(row["protein"].values[0]) + "\n"
+        output += "calories: " + str(row["calories"].values[0]) + ' kcal' + "\n"
+        output += "carbs: " + str(row["carbohydrate"].values[0]) + "\n"
+        output += "fats: " + str(row["total_fat"].values[0]) + "\n"
 
+    return output
 
 
