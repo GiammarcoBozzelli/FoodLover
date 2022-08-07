f557651cfdc5829632d0fee1d469a167be078ab1
GiammarcoBozzelli <giammarcobozzelli3006@gmail.com>
2022-08-07 09:53:41 +0200

    minor adjustments

--- input.py
+++ input.py
@@ -5,22 +5,21 @@ def parse_arguments():
     '''Parse command arguments'''
     parser = argparse.ArgumentParser(description='Retrieve food name'
                                      + 'input from the users')
-    parser.add_argument('food', action='store', nargs='+', help='Input the '
+    parser.add_argument('food', action='store', nargs='*', help='Input the '
                         + 'name of the food you would like to know more about',
                         type=str)
-    parser.add_argument('-v', action='store_true', help='Increase the '
-                        + ' verbosity of the outputs',)
-    parser.add_argument('-s', action='store_true', help='Add -s to your input'
-                        + 'to show all foods in the FoodLover database')
-    parser.add_argument('-a', action='store_true', help='Add a food to the '
-                        + 'database')
-    parser.add_argument('-p', action='store_true', help='Displays how many '
-                        + 'grams of proteins the food has per 100 grams')
-    parser.add_argument('-cal', action='store_true', help='Displays how many '
-                        + 'calories the food has per 100 grams')
-    parser.add_argument('-f', action='store_true', help='Displays how many '
-                        + 'grams of fats the food has per 100 grams')
-    parser.add_argument('-c', action='store_true', help='Displays how many '
-                        + 'grams of carbohydrates the food has per 100 grams')
+    parser.add_argument('-s', '--show', action='store_true', help='Add -s to '
+                        + 'your input to show all foods in the database')
+    parser.add_argument('-a', '--add', action='store_true', help='Add a food '
+                        + 'to the database')
+    parser.add_argument('-p', '--protein', action='store_true', help='Displays'
+                        + ' how many grams of proteins the food has per 100g')
+    parser.add_argument('-cal', '--calories', action='store_true', help=''
+                        + 'Displays how many calories the food has per 100g')
+    parser.add_argument('-f', '--fats', action='store_true', help='Displays '
+                        + 'how many grams of fats the food has per 100 grams')
+    parser.add_argument('-c', '--carbs', action='store_true', help='Displays '
+                        + 'how many grams of carbohydrates the food has '
+                        + 'per 100g')
 
     return parser.parse_args()
