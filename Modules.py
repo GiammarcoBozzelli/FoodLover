020b5df29aae1929f6db46af0a822578411daabc
GiammarcoBozzelli <giammarcobozzelli3006@gmail.com>
2022-08-07 15:26:47 +0200

    Created a new Modules.py
    We will use this file to contain all our functions

---
+++ Modules.py
@@ -0,0 +1,69 @@
+
+import argparse
+from matplotlib.pyplot import show
+import pandas as pd
+import numpy as np
+from csv import DictWriter
+
+
+def parse_arguments():
+    '''Parse command arguments'''
+    parser = argparse.ArgumentParser(description='Retrieve food name'
+                                     + 'input from the users')
+    parser.add_argument('food', action='store', nargs='*', help='Input the '
+                        + 'name of the food you would like to know more about',
+                        type=str)
+    parser.add_argument('-s', '--show', action='store_true', help='Add -s to '
+                        + 'your input to show all foods in the database')
+    parser.add_argument('-a', '--add', action='store_true', help='Add a food '
+                        + 'to the database')
+    parser.add_argument('-p', '--protein', action='store_true', help='Displays'
+                        + ' how many grams of proteins the food has per 100g')
+    parser.add_argument('-cal', '--calories', action='store_true', help=''
+                        + 'Displays how many calories the food has per 100g')
+    parser.add_argument('-f', '--fats', action='store_true', help='Displays '
+                        + 'how many grams of fats the food has per 100 grams')
+    parser.add_argument('-c', '--carbs', action='store_true', help='Displays '
+                        + 'how many grams of carbohydrates the food has '
+                        + 'per 100g')
+
+    return parser.parse_args()
+
+
+def check_input(args):
+    '''Checks that the input food is made of only accepted characters
+       and if it is present in the database if not, it returns an
+       error message to the user'''
+    food_name = ' '.join(args.food).lower()
+    df = read_dataframe('nutrition.csv')
+    
+    if args.show:
+        show_food(df)
+        quit()
+    elif args.food == []:
+        print('Type the food you are interested in after \"main.py\"')
+        quit()
+
+    if all(x.isalpha() or x.isspace() for x in food_name):
+        if args.add:
+            if food_name in df.values:
+                print('This food is already in the database! \nType '
+                      + 'again the command without -a to get information'
+                      + f' about \"{food_name}\"')
+                return False
+            else:
+                return True
+        else:
+            if food_name in df.values:
+                return True
+            else:
+                print('We are sorry but this food is not in our database :('
+                      + '\nYou can help us out and fill it yourself!\n'
+                      + 'All you have to do is relaunching the progra'
+                      + 'adding \"-a\" to your command!')
+                return False
+    else:
+        print('There is something wrong with your food name... Try again '
+              + 'with something like \'Apple\' and avoid using special'
+              + ' characters or numbers!')
+        return False
