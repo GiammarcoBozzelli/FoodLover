131d8e742334ba8117f749106dd9492dfec7ec96
Angelica Habib <angelicahabib@Mac-di-Angelica.local>
2022-08-07 15:18:48 +0200

    issue #5

--- Choice.py
+++ Choice.py
@@ -1,5 +1,7 @@
 
-def showFood(df, letter):
+def show_food(df):
+    letter = input('Type the starting letters of the food you are looking for: \n')
     out = df[df['name'].str.startswith(letter)]     #tutti i cibi che iniziano con la lettera specificata dall'utente (letter)
-    out=out.reset_index(drop=True)                  #reset degli indici delle righe, numerati  1, 2, ..., n
-    return out['name']                              #return solamente della colonna name
+    out = out.reset_index(drop=True)                  #reset degli indici delle righe, numerati  1, 2, ..., n
+    print(out['name'])                              #return solamente della colonna name
+    return
