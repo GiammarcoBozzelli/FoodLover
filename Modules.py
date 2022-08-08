3865c99296e72294907599723324d62ddd44e871
Chiara Dalto <chiaradalto@Air-di-Chiara-2.home-life.hub>
2022-08-08 15:39:03 +0200

    optimized the code and added docrstrings. Checked PEP 8 protocol

--- Modules.py
+++ Modules.py
@@ -1,12 +1,11 @@
-
 import argparse
 import pandas as pd
-import numpy as np
-from csv import DictWriter
+
 
 def has_numbers(input_string):
     """Return False if the string contains only letters"""
     return any(char.isdigit() for char in input_string)
+
 
 def try_parse_int(num):
     """Try to parse a string in an intger; return True if parsing works"""
@@ -46,41 +45,41 @@ def name_input():
         return user_in
 
 
-def number_input(property, is_float):
+def number_input(nutvalues, is_float):
     """Control user input for food properties (i.e. fat, calories, carbs, ...)"""
     stop = False
 
     if is_float:
         # property is a float number
-        if try_parse_float(user_in := input(f"Type food {property}: ")):
+        if try_parse_float(user_in := input(f"Type food {nutvalues}: ")):
             # property is a float number
             return user_in
         else:
             # property contains illegal character(s)
-            print("\n", f"You are allowed only to use floats for {property}, TRY AGAIN!", "\n")
+            print("\n", f"You are allowed only to use floats for {nutvalues}, TRY AGAIN!", "\n")
             while not stop:
                 # loop for input only correct digits
-                if try_parse_float(user_in := input(f"Type food {property}: ")):
+                if try_parse_float(user_in := input(f"Type food {nutvalues}: ")):
                     stop = True
                 else:
-                    print("\n", f"You are allowed only to use floats for {property}, TRY AGAIN!", "\n")
+                    print("\n", f"You are allowed only to use floats for {nutvalues}, TRY AGAIN!", "\n")
 
             return user_in
 
     else:
         # property is an intger number
-        if try_parse_int(user_in := input(f"Type food {property}: ")):
+        if try_parse_int(user_in := input(f"Type food {nutvalues}: ")):
             # property is an integer number
             return user_in
         else:
             # property contains illegal character(s)
-            print("\n", f"You are allowed only to use integers for {property}, TRY AGAIN!", "\n")
+            print("\n", f"You are allowed only to use integers for {nutvalues}, TRY AGAIN!", "\n")
             while not stop:
                 # loop for input only correct digits
-                if try_parse_int(user_in := input(f"Type food {property}: "), is_float):
+                if try_parse_int(user_in := input(f"Type food {nutvalues}: "), is_float):
                     stop = True
                 else:
-                    print("\n", f"You are allowed only to use integers for {property}, TRY AGAIN!", "\n")
+                    print("\n", f"You are allowed only to use integers for {nutvalues}, TRY AGAIN!", "\n")
 
             return user_in
 
@@ -94,11 +93,11 @@ def update_dataframe(df):
     if user_choice == "Y" or user_choice == "y" or user_choice == "":
         # user decides to modify CSV file
         food_properties["name"] = ["" + str(name_input()) + ""]
-        food_properties["carbohydrate"] = [str(number_input("carbs", is_float = True)) + "g"]
-        food_properties["total_fat"] = [str(number_input("fats", is_float = True)) + "g"]
-        food_properties["protein"] = [str(number_input("proteins", is_float = True)) + " g"]
-        food_properties["calories"] = [int(number_input("calories", is_float = False))]
-        # collect all necessary dat
+        food_properties["carbohydrate"] = [str(number_input("carbs", is_float=True)) + "g"]
+        food_properties["total_fat"] = [str(number_input("fats", is_float=True)) + "g"]
+        food_properties["protein"] = [str(number_input("proteins", is_float=True)) + " g"]
+        food_properties["calories"] = [int(number_input("calories", is_float=False))]
+        # collect all necessary data
         df_from_dict = pd.DataFrame.from_dict(food_properties)
         df = pd.concat([df, df_from_dict], ignore_index=True)
         df.index = range(0, len(df["name"]))
@@ -120,23 +119,25 @@ def read_dataframe(file_name):
 
     return df.copy()
 
+
 def get_info(arguments):
+    '''search for the correct output to return according the user input searching on the csv file'''
     df = read_dataframe('nutrition.csv')
-    row = df.loc[df['name'] == arguments.food]  # ottengo la riga corrispondente al food specificato dall'utente
+    row = df.loc[df['name'] == arguments.food]  # row corresponding to the food name given as input
     output = ''
-    if arguments.show == True:
+    if arguments.show:
         show_food(df)
         return None
-    if arguments.add == True:
+    if arguments.add:
         update_dataframe(df)
         return None
-    if arguments.protein == True:
+    if arguments.protein:
         output += "protein: " + str(row["protein"].values[0]) + "\n"
-    if arguments.calories == True:
+    if arguments.calories:
         output += "calories: " + str(row["calories"].values[0]) + ' kcal' + "\n"
-    if arguments.carbs == True:
+    if arguments.carbs:
         output += "carbs: " + str(row["carbohydrate"].values[0]) + "\n"
-    if arguments.fats == True:
+    if arguments.fats:
         output += "fats: " + str(row["total_fat"].values[0]) + "\n"
     if not arguments.add and not arguments.protein and not arguments.carbs and not arguments.fats and not arguments.calories and not arguments.show:
         output += "protein: " + str(row["protein"].values[0]) + "\n"
@@ -147,26 +148,27 @@ def get_info(arguments):
     print(f"Name: {arguments.food}\n" + output)
     return None
 
+
 def parse_arguments():
     '''Parse command arguments'''
     parser = argparse.ArgumentParser(description='Retrieve food name'
-                                     + 'input from the users')
+                                                 + 'input from the users')
     parser.add_argument('food', action='store', nargs='*', help='Input the '
-                        + 'name of the food you would like to know more about',
+                                                                + 'name of the food you would like to know more about',
                         type=str)
     parser.add_argument('-s', '--show', action='store_true', help='Add -s to '
-                        + 'your input to show all foods in the database')
+                                                                  + 'your input to show all foods in the database')
     parser.add_argument('-a', '--add', action='store_true', help='Add a food '
-                        + 'to the database')
+                                                                 + 'to the database')
     parser.add_argument('-p', '--protein', action='store_true', help='Displays'
-                        + ' how many grams of proteins the food has per 100g')
+                                                                     + ' how many grams of proteins the food has per 100g')
     parser.add_argument('-cal', '--calories', action='store_true', help=''
-                        + 'Displays how many calories the food has per 100g')
+                                                                        + 'Displays how many calories the food has per 100g')
     parser.add_argument('-f', '--fats', action='store_true', help='Displays '
-                        + 'how many grams of fats the food has per 100 grams')
+                                                                  + 'how many grams of fats the food has per 100 grams')
     parser.add_argument('-c', '--carbs', action='store_true', help='Displays '
-                        + 'how many grams of carbohydrates the food has '
-                        + 'per 100g')
+                                                                   + 'how many grams of carbohydrates the food has '
+                                                                   + 'per 100g')
 
     return parser.parse_args()
 
@@ -209,9 +211,12 @@ def check_input(args):
               + ' characters or numbers!')
         return False
 
+
 def show_food(df):
+    '''shows to the user all the food that starts with the letters he has typed'''
     letter = input('Type the starting letters of the food you are looking for: \n')
-    out = df[df['name'].str.startswith(letter)]     #tutti i cibi che iniziano con la lettera specificata dall'utente (letter)
-    out = out.reset_index(drop=True)                  #reset degli indici delle righe, numerati  1, 2, ..., n
-    print(out['name'])                              #return solamente della colonna name
+    out = df[
+        df['name'].str.startswith(letter)]
+    out = out.reset_index(drop=True)
+    print(out['name'])
     return
