b323624a49b11081016fc803e182759ac3a2f60a
GiammarcoBozzelli <giammarcobozzelli3006@gmail.com>
2022-08-09 13:06:58 +0200

    improved tests

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 
 import unittest
 from Modules import *
+import pandas.testing as pd_testing
 
 
 class TestInputsValues(unittest.TestCase):
@@ -42,7 +43,7 @@ class TestInputsValues(unittest.TestCase):
         The function test if passed a food present in the
         progam is able to detect the input as valid
         '''
-        #simulate user input
+        # simulate user input
         self.arguments.food = ['ciao!32']
         with suppress_stdout():
             out = check_input(self.arguments)
@@ -88,11 +89,53 @@ class TestInputsValues(unittest.TestCase):
         output = f"Name: {self.arguments.food}\n" + output
         self.assertEqual(get_info(self.arguments), output)
 
-    def test_add(self):
+    def test_show(self):
+        '''
+        Checks if the program correctly shows all foods
+        starting with the letters specified by the users
+        '''
         pass
 
-    def test_show(self):
-        pass
+    def test_update_dataframe(self):
+        '''
+        Checks if the function correctly writes a new line
+        '''
+        # copied the function update_dataframe()
+        # to bypass input requirements
+        food_properties = dict()
+        # user decides to modify CSV file
+        food_properties["name"] = ['test']
+        food_properties["carbohydrate"] = ['20' + "g"]
+        food_properties["total_fat"] = ['20' + "g"]
+        food_properties["protein"] = ['20' + "g"]
+        food_properties["calories"] = ['20']
+        # collect all necessary data
+        df_from_dict = pd.DataFrame.from_dict(food_properties)
+        df = pd.concat([self.df, df_from_dict], ignore_index=True)
+        df.index = range(0, len(df["name"]))
+        df = df[df.columns.drop(list(df.filter(regex="Unnamed")))]
+        df.to_csv("nutrition.csv")
+        df = read_dataframe('nutrition.csv')
+
+        expected_row = pd.DataFrame(columns=df.columns).fillna('')
+        expected_row['name'] = ['test']
+        expected_row["carbohydrate"] = ['20' + "g"]
+        expected_row["total_fat"] = ['20' + "g"]
+        expected_row["protein"] = ['20' + "g"]
+        expected_row["calories"] = [int(20)]
+
+        added_row = df.iloc[-1:]
+
+        pd_testing.assert_frame_equal(added_row[['name', "total_fat",
+                                                 'carbohydrate',
+                                                 'protein',
+                                                 'calories']].reset_index(
+                                                    drop=True),
+
+                                      expected_row[['name', "total_fat",
+                                                    'carbohydrate', 'protein',
+                                                    'calories']].reset_index(
+                                                        drop=True))
 
 
 if __name__ == "__main__":
